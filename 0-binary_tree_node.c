a7faade00ac06f332a79982b125bda3dc7fb598e
Immaannn2222 <imennaayari@gmail.com>
2020-07-23 17:22:25 +0100

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *root;
 root = malloc(sizeof(binary_tree_t));
+if (root == NULL)
+return (NULL);
 root->parent = parent;
 root->n = value;
 root->left = NULL;
